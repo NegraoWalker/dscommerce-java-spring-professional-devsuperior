8bfe4e7468bae96f1882b4cf73292c360b8621cd
Walker Negrao <1882452@tcs.com>
2025-04-24 07:39:10 -0300

    Criação do repositório

---
+++ src/main/java/com/walker/dscommerce/DscommerceJavaSpringProfessionalDevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.walker.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceJavaSpringProfessionalDevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceJavaSpringProfessionalDevsuperiorApplication.class, args);
+	}
+
+}
