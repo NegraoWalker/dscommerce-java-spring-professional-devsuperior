e79667a0d0f408cc98dffef6c99d4b5ecb1e9b08
Walker <walker.esteves.negrao@gmail.com>
2025-12-31 14:08:59 -0300

    Atualização

---
+++ dscommerce-secao5/src/main/java/com/walker/dscommerce/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.walker.dscommerce.controller;
+
+import com.walker.dscommerce.dto.UserDTO;
+import com.walker.dscommerce.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_CLIENT','ROLE_ADMIN')")
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDTO> findUserMe() {
+        UserDTO userDTO = userService.getUserMe();
+        return ResponseEntity.ok(userDTO);
+    }
+}
