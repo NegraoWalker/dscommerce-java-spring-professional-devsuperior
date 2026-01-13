60f38d2df1550cb8e86dadb1decaa1cbeb28facc
Walker <walker.esteves.negrao@gmail.com>
2026-01-13 20:08:33 -0300

    Atualização

---
+++ dscommerce-secao5/src/main/java/com/walker/dscommerce/controller/CategoryController.java
@@ -0,0 +1,26 @@
+package com.walker.dscommerce.controller;
+
+import com.walker.dscommerce.dto.CategoryDTO;
+import com.walker.dscommerce.service.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/api/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> categoryDTOS = categoryService.findAll();
+        return ResponseEntity.ok(categoryDTOS);
+    }
+
+}
---
+++ dscommerce-secao5/src/main/java/com/walker/dscommerce/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.walker.dscommerce.repository;
+
+import com.walker.dscommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+}
